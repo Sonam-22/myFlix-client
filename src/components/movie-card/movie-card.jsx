3261bceed02e908e456e15527352e9c0d1106890
Sonam-22 <sonampriya7070@gmail.com>
2022-05-11 12:44:28 +0200

    adds a feature to add favourite movies

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,21 +1,78 @@
 import React from "react";
 import PropTypes from "prop-types";
+
+import { Link } from "react-router-dom";
+import axios from "axios";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import "./movie-card.scss";
+import { Badge } from "react-bootstrap";
 
 export class MovieCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      favourite: false,
+    };
+  }
+
+  componentDidMount() {
+    const { movie } = this.props;
+    const user = JSON.parse(localStorage.getItem("user"));
+    this.setState({
+      favourite: user.favouriteMovies.includes(movie._id),
+    });
+  }
+
+  addToFavourite() {
+    const { movie, onUserUpdated } = this.props;
+    const token = localStorage.getItem("token");
+    const user = JSON.parse(localStorage.getItem("user"));
+    axios
+      .post(
+        `https://rocky-fortress-51190.herokuapp.com/users/${user.userName}/movies/${movie._id}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((response) => {
+        this.setState({
+          favourite: true,
+        });
+        user.favouriteMovies.push(movie._id);
+        onUserUpdated(user);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
   render() {
-    const { movie, onMovieClick } = this.props;
+    const { movie } = this.props;
     return (
-      <Card onClick={() => onMovieClick(movie)}>
+      <Card>
         <Card.Img variant="top" src={movie.ImagePath} />
         <Card.Body>
-          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Title>
+            {movie.Title}
+            {this.state.favourite && (
+              <Badge className="ms-2" bg="success">
+                Favourite
+              </Badge>
+            )}
+          </Card.Title>
           <Card.Text>{movie.Description}</Card.Text>
-          <Button onClick={() => onMovieClick(movie)} variant="link">
+          <Button className="me-2" href={`/movies/${movie._id}`}>
             Open
           </Button>
+          {!this.state.favourite && (
+            <Button onClick={() => this.addToFavourite()}>
+              Add to favourite
+            </Button>
+          )}
         </Card.Body>
       </Card>
     );
@@ -33,5 +90,4 @@ MovieCard.propTypes = {
     }),
     ImagePath: PropTypes.string.isRequired,
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
 };
