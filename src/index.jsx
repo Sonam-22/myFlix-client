64bef0457692ddb8e0a753f62d3eb3a1b88adf6e
Sonam-22 <sonampriya7070@gmail.com>
2022-05-11 12:48:59 +0200

    refactored  and deletes navbar codes

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import ReactDom from "react-dom";
 import { MainView } from "./components/main-view/main-view";
-import { Navbar, Container } from "react-bootstrap";
 
 //import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
@@ -9,26 +8,12 @@ import "./index.scss";
 //Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-    return (
-      <Fragment>
-        <Navbar bg="dark">
-          <Container>
-            <Navbar.Brand href="#">
-              <h1 className="text-white fst-italic fw-bolder">MyFlix</h1>
-            </Navbar.Brand>
-          </Container>
-        </Navbar>
-        <MainView />
-        <footer>
-          <div className="p-3 bg-dark text-white">&copy;2022 Sonam Priya</div>
-        </footer>
-      </Fragment>
-    );
+    return <MainView />;
   }
 }
 
 //Finds the root of your app
-const container = document.getElementsByClassName("app-container")[0];
+const container = document.getElementById("app-container");
 
 //tells react to render your app in the root Dom element
-ReactDom.render(React.createElement(MyFlixApplication), container);
+ReactDom.render(<MyFlixApplication />, container);
