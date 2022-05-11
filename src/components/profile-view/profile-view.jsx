45e865198c242d07d4d310936ea8d2309cfb2870
Sonam-22 <sonampriya7070@gmail.com>
2022-05-11 12:27:25 +0200

    Added profile view
     updates user info, adds user's favorite movies, remove a movie from  list of favorites and user to deregister.

---
+++ src/components/profile-view/profile-view.jsx
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from "react";
+import { Button, Col, Row, Card, Container } from "react-bootstrap";
+import axios from "axios";
+import "./profile-view.scss";
+import { Nav } from "react-bootstrap";
+
+import { UserData } from "./user-data";
+import { UpdateUserForm } from "./update-user";
+import { FavoriteMovies } from "./favourite-movies";
+
+export function ProfileView(props) {
+  const { onUserUpdated } = props;
+  const [userdata, setUserdata] = useState(props.user);
+  const [favoriteMoviesList, setFavoriteMoviesList] = useState([
+    ...props.movies.filter((m) => props.user.favouriteMovies.includes(m._id)),
+  ]);
+
+  const token = localStorage.getItem("token");
+  const currentUser = JSON.parse(localStorage.getItem("user"));
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .put(
+        `https://rocky-fortress-51190.herokuapp.com/users/${currentUser.userName}`,
+        userdata
+      )
+      .then((response) => {
+        alert("Profile updated");
+        onUserUpdated(null);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const handleUpdate = (e) => {
+    setUserdata({
+      ...userdata,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const deleteProfile = (e) => {
+    axios
+      .delete(
+        `https://rocky-fortress-51190.herokuapp.com/users/${userdata.userName}`
+      )
+      .then((response) => {
+        alert("Your profile has beeen deleted");
+        onUserUpdated(null);
+
+        window.open("/", "_self");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  const removeFav = (id) => {
+    axios
+      .delete(
+        `https://rocky-fortress-51190.herokuapp.com/users/${currentUser.userName}/movies/${id}`
+      )
+      .then(() => {
+        const newFavourites = favoriteMoviesList.filter(
+          (movie) => movie._id != id
+        );
+        const currentUser = JSON.parse(localStorage.getItem("user"));
+        currentUser.favouriteMovies = currentUser.favouriteMovies.filter(
+          (fmId) => id !== fmId
+        );
+        setFavoriteMoviesList(newFavourites);
+        onUserUpdated(currentUser);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col med={4}>
+          {/* Display userdata */}
+          <UserData userdata={userdata} />
+
+          {/* List of favorite movies */}
+          <div className="mb-2">
+            <FavoriteMovies
+              favoriteMoviesList={favoriteMoviesList}
+              removeFav={removeFav}
+            />
+          </div>
+
+          {/* Form to update user data */}
+          <UpdateUserForm
+            userdata={userdata}
+            handleSubmit={handleSubmit}
+            handleUpdate={handleUpdate}
+          />
+
+          {/* Button to delete user */}
+          <div className="d-flex flex-row gap-4 my-4 justify-content-center align-items-center">
+            <Button variant="danger" type="submit" onClick={deleteProfile}>
+              Delete Profile
+            </Button>
+            <Nav.Link href="/">Back to Movies</Nav.Link>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
